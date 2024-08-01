44238fa5ff3f4555ba0a9ae1a4cd2edfffb1b4c1
nimbusWSL <medagowtham97@gmail.com>
2024-08-01 23:06:38 +0530

    Added a working test case for  nginx

--- _1.2.2_nginx_ingress/main.tf
+++ _1.2.2_nginx_ingress/main.tf
@@ -1,4 +1,4 @@
-resource "helm_release" "ingress-nginx" {
+resource "helm_release" "ingress_nginx" {
   name      = "nginx-ingress"
   namespace = var.namespace
 
@@ -17,12 +17,45 @@ resource "helm_release" "ingress-nginx" {
     name  = "controller.replicaCount"
     value = "1"
   }
+
   set {
-    name  = "controller.service.internal.annotations.service\\.ingress\\.kubernetes\\.io/subnets"
-    value = yamlencode(data.terraform_remote_state.eks.outputs.private_subnets)
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-name"
+    value = "extdns-tls-lbc-network-lb"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    value = "external"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/load-balancer-source-ranges"
+    value = "0.0.0.0/0"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    value = "internet-facing"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
+    value = "arn:aws:acm:us-east-1:058264194719:certificate/442d0a9c-5bb3-43ba-8ad2-27e53a40f0f6"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
+    value = "443"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-negotiation-policy"
+    value = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"
+    value = "tcp"
   }
 
 }
-
-
-#  helm install ingress-nginx ingress-nginx/ingress-nginx --set controller.publishService.enabled=true  --set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-type"="nlb"
--- _1.2.2_nginx_ingress/outputs.tf
+++ _1.2.2_nginx_ingress/outputs.tf
@@ -5,24 +5,24 @@ output "eks_private_subnets" {
 
 # output "ingress_nginx_helm_metadata" {
 #   description = "Raw Metadata Block outlining status of the deployed release."
-#   value       = helm_release.ingress-nginx.metadata
+#   value       = helm_release.ingress_nginx.metadata
 # }
 
 
 output "decoded_ingress_nginx_values" {
   description = "Decoded values field from ingress-nginx Helm release metadata."
-  value       = jsondecode(helm_release.ingress-nginx.metadata[0].values)
+  value       = jsondecode(helm_release.ingress_nginx.metadata[0].values)
 }
 
 output "ingress_nginx_helm_metadata" {
   description = "Decoded Metadata Block outlining status of the deployed release."
   value = {
-    app_version = helm_release.ingress-nginx.metadata[0].app_version
-    chart       = helm_release.ingress-nginx.metadata[0].chart
-    name        = helm_release.ingress-nginx.metadata[0].name
-    namespace   = helm_release.ingress-nginx.metadata[0].namespace
-    revision    = helm_release.ingress-nginx.metadata[0].revision
-    values      = jsondecode(helm_release.ingress-nginx.metadata[0].values)
-    version     = helm_release.ingress-nginx.metadata[0].version
+    app_version = helm_release.ingress_nginx.metadata[0].app_version
+    chart       = helm_release.ingress_nginx.metadata[0].chart
+    name        = helm_release.ingress_nginx.metadata[0].name
+    namespace   = helm_release.ingress_nginx.metadata[0].namespace
+    revision    = helm_release.ingress_nginx.metadata[0].revision
+    values      = jsondecode(helm_release.ingress_nginx.metadata[0].values)
+    version     = helm_release.ingress_nginx.metadata[0].version
   }
 }
