3af3f8e550383e44517f3c508e89cf5258235221
nimbusWSL <medagowtham97@gmail.com>
2024-08-02 09:57:23 +0530

    loki change

--- _4_loki/main.tf
+++ _4_loki/main.tf
@@ -7,8 +7,8 @@ resource "helm_release" "loki" {
   create_namespace = true
 
   values = [
-    templatefile("${path.module}/manifests/test.yaml", {
-      role_arn = data.terraform_remote_state.s3.outputs.irsa_s3_iam_role_arn
+    templatefile("${path.module}/manifests/def.yaml", {
+
       }
   )]
   # set {
