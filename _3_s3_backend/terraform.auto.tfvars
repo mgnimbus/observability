a0d5e42d4e67740528df00ac1fadbe8bf7ff5e1f
Gowtham <medagowtham2@yahoo.com>
2025-03-30 04:33:44 +0530

    working mimir

--- _3_s3_backend/terraform.auto.tfvars
+++ _3_s3_backend/terraform.auto.tfvars
@@ -4,5 +4,5 @@ region           = "us-east-1"
 environment      = "dev"
 business_divsion = "meda"
 
-namespace            = "grafana"
-service_account_name = "s3-sa"
+namespace            = "mimir"
+service_account_name = "mimir-s3"
--- _4_loki/main.tf
+++ _4_loki/main.tf
@@ -5,9 +5,10 @@ resource "helm_release" "loki" {
   chart            = "loki"
   namespace        = var.namespace
   create_namespace = true
+  timeout          = 120
 
   values = [
-    templatefile("${path.module}/manifests/loki/default.yaml", {
+    templatefile("${path.module}/manifests/loki/test.yaml", {
       role_name          = "loki-sa"
       role_arn           = aws_iam_role.irsa_s3_role.arn
       loki_ruler_bucket  = "meda-dev-mackerel-loki-ruler"
