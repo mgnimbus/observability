002f6e912e829205a2c6536570481f0a750d12b9
Gowtham <medagowtham2@yahoo.com>
2025-03-31 19:48:59 +0530

    cert trail temp

--- _1.4_cert/ca/cmd.sh
+++ _1.4_cert/ca/cmd.sh
@@ -4,3 +4,5 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \                           
   -addext "subjectAltName = DNS:zues.com"
 
 # openssl x509 -in zues.crt -noout -serial 
+# base64 -w 0 zues.crt > encoded-crt.txt      
+# base64 -w 0 zues.key > encoded-key.txt 
